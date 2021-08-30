6b71b358d5055638c1b845a22c8c21140a0459b2
Nadia <nadiyaklokova@gmail.com>
2021-08-30 08:53:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('.container_menu')
+const background = document.querySelector('.background')
+const menu = document.querySelector('.menu');
+
+
+
+btn.addEventListener('click',() =>{
+background.classList.toggle('active');
+menu.classList.toggle('active');
+
+
+let glowInTexts = document.querySelectorAll(".glowIn");
+glowInTexts.forEach(glowInText => {
+  let letters = glowInText.textContent.split("");
+  glowInText.textContent = "";
+  letters.forEach((letter, i) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.style.animationDelay = `${i * 0.05}s`;
+    glowInText.append(span);
+  });
+});
+
+} )
+
+
