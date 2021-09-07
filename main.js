926e18fe7f8fd0f600a60e66cc52bac1cfce095f
Nadia <nadiyaklokova@gmail.com>
2021-09-08 00:20:44 +0200

    cerebro, brillo, data-target

--- main.js
+++ main.js
@@ -26,3 +26,19 @@ glowInTexts.forEach(glowInText => {
 } )
 
 
+/* ---------------pestañas-----------------tabs-------------- */
+
+const targets = document.querySelectorAll('[data-target]');
+const content = document.querySelectorAll('[data-content]')
+
+targets.forEach(target =>{
+    target.addEventListener('click',() =>{
+        content.forEach(c=>{
+            c.classList.remove('active') 
+        })
+        const t = document.querySelector(target.dataset.target)
+        t.classList.add('active') 
+    })
+})
+
+
